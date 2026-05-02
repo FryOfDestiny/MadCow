23ea1a0fd93b062649db4fa0887980cf2ad202fc
Logsty03 <doutlick90@gmail.com>
2026-05-02 16:34:32 -0400

    delete comments

--- XP_drop.gd
+++ XP_drop.gd
@@ -4,10 +4,8 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		# 1. Print all children to see what the node is actually called
 		print("Player children: ", body.get_children())
 		
-		# 2. Try to find the stats node
 		var stats = body.get_node_or_null("Stats") 
 		
 		if stats:
@@ -15,8 +13,7 @@ func _on_body_entered(body: Node2D) -> void:
 			stats.add_experience(xp_amount)
 			queue_free()
 		else:
-			print("ERROR: Could not find a node named 'Stats' on the player.")
-			# Alternative: Try to find by class if the name is different
+	
 			for child in body.get_children():
 				if child is Stats:
 					print("Found stats by class! Adding XP...")
